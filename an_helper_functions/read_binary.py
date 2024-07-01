315162c76b1a1721f9c8ffa87d0f518a04e30c05
Margaret Murakami <mmurakami@utexas.edu>
2024-07-01 16:20:59 -0500

    fixed Pemb fig 8c, attempts at fig 8b but not complete

--- an_helper_functions/read_binary.py
+++ an_helper_functions/read_binary.py
@@ -1,4 +1,7 @@
 import numpy as np
+import sys
+sys.path.append("/home/mmurakami/MITgcm/MITgcm_c68r/MITgcm-checkpoint68r/utils/python/MITgcmutils/MITgcmutils/")
+from mds import *
 
 def read_float32(fileIn):
     with open(fileIn, 'rb') as f:
@@ -30,3 +33,39 @@ def read_float64_skip(fileIn,recordLen,recordNo):
         bytes = file.read(recordLen*8)
         fldout = np.frombuffer(bytes, dtype=np.dtype('>f8')).copy()
     return fldout
+
+# write abbreviation functions
+def read_field3d(tsstr,nz,ny,nx,file_name,varnames,mymsk,dirIn):
+    FIELD = np.full((len(tsstr),nz,ny,nx),np.nan)
+    
+    meta_set = parsemeta(dirIn + file_name + "." + tsstr[0] + ".meta")
+    fldlist = np.array(meta_set["fldList"])
+    recs = np.array([])
+    for var in varnames:
+        irec = np.where(fldlist == var)
+        recs = np.append(recs, irec[0][0])
+        
+    for i in range(len(tsstr)):
+        read = [int(tsstr[i])]
+        FIELDi, its, meta = rdmds(dirIn + '/' + file_name, read, returnmeta=True, rec=recs[0])
+        FIELD[i, :, :, :] = np.reshape(FIELDi, (nz, ny, nx)) * mymsk[np.newaxis, :, :]
+
+    return FIELD
+
+# write abbreviation functions
+def read_field2d(tsstr,ny,nx,file_name,varnames,mymsk,dirIn):
+    FIELD = np.full((len(tsstr),ny,nx),np.nan)
+    
+    meta_set = parsemeta(dirIn + file_name + "." + tsstr[0] + ".meta")
+    fldlist = np.array(meta_set["fldList"])
+    recs = np.array([])
+    for var in varnames:
+        irec = np.where(fldlist == var)
+        recs = np.append(recs, irec[0][0])
+        
+    for i in range(len(tsstr)):
+        read = [int(tsstr[i])]
+    
+        FIELDi,its,meta = rdmds(dirIn + '/' + file_name,read,returnmeta=True,rec=recs[0])
+        FIELD[i,:,:] = np.reshape(FIELDi,(ny,nx)) * mymsk[:,:]
+    return FIELD
